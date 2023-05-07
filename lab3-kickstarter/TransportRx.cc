2627c2a53f6f0b2b27b941305c21367e03eeb352
crismuz <crismuz1999@gmail.com>
2023-05-07 16:07:40 -0300

    Comienzo parte Diseno

---
+++ lab3-kickstarter/TransportRx.cc
@@ -0,0 +1,80 @@
+#ifndef TRANSPORTRX
+#define TRANSPORTRX
+
+#include <string.h>
+#include <omnetpp.h>
+
+#include "FeedBackPacket_m.h"
+
+using namespace omnetpp;
+
+class TransportRx: public cSimpleModule {
+private:
+    cQueue buffer;
+    cMessage *endServiceEvent;
+    simtime_t serviceTime;
+    cOutVector bufferSizeVector;
+    cOutVector packetDropVector;
+public:
+    TransportRx();
+    virtual ~TransportRx();
+protected:
+    virtual void initialize();
+    virtual void finish();
+    virtual void handleMessage(cMessage *msg);
+};
+
+Define_Module(TransportRx);
+
+TransportRx::TransportRx() {
+    endServiceEvent = NULL;
+}
+
+TransportRx::~TransportRx() {
+    cancelAndDelete(endServiceEvent);
+}
+
+void TransportRx::initialize() {
+    buffer.setName("buffer");
+    endServiceEvent = new cMessage("endService");
+    bufferSizeVector.setName("Buffer Size");
+    packetDropVector.setName("Packet drops");
+}
+
+void TransportRx::finish() {
+}
+
+void TransportRx::handleMessage(cMessage *msg) {
+
+    // if msg is signaling an endServiceEvent
+    if (msg == endServiceEvent) {
+        // if packet in buffer, send next one
+        if (!buffer.isEmpty()) {
+            // dequeue packet
+            cPacket *pkt = (cPacket*) buffer.pop();
+            // send packet
+            send(pkt, "toOut$o");
+            // start new service
+            serviceTime = pkt->getDuration();
+            scheduleAt(simTime() + serviceTime, endServiceEvent);
+        }
+    // check buffer limit
+    } else if (buffer.getLength() >= par("bufferSize").intValue()) {
+        // drop the packet
+        delete msg;
+        this->bubble("packet dropped");
+        packetDropVector.record(1);
+    } else {
+        // enqueue the packet
+        buffer.insert(msg);
+        bufferSizeVector.record(buffer.getLength());
+        // if the server is idle
+        if (!endServiceEvent->isScheduled()) {
+            // start the service now
+            scheduleAt(simTime() + 0, endServiceEvent);
+        }
+    }
+
+}
+
+#endif /* TransportRx */
---
+++ lab3-kickstarter/TransportTx.cc
@@ -0,0 +1,93 @@
+#ifndef TRANSPORTTX
+#define TRANSPORTTX
+
+#include <string.h>
+#include <omnetpp.h>
+
+#include "FeedBackPacket_m.h"
+
+
+using namespace omnetpp;
+
+class TransportTx: public cSimpleModule {
+private:
+    cQueue buffer;
+    cMessage *endServiceEvent;
+    simtime_t serviceTime;
+    cOutVector bufferSizeVector;
+    cOutVector packetDropVector;
+public:
+    TransportTx();
+    virtual ~TransportTx();
+protected:
+    virtual void initialize();
+    virtual void finish();
+    virtual void handleMessage(cMessage *msg);
+};
+
+Define_Module(TransportTx);
+
+TransportTx::TransportTx() {
+    endServiceEvent = NULL;
+}
+
+TransportTx::~TransportTx() {
+    cancelAndDelete(endServiceEvent);
+}
+
+void TransportTx::initialize() {
+    buffer.setName("buffer");
+    endServiceEvent = new cMessage("endService");
+    bufferSizeVector.setName("Buffer Size");
+    packetDropVector.setName("Packet drops");
+}
+
+void TransportTx::finish() {
+}
+
+void TransportTx::handleMessage(cMessage *msg) {
+    // msg is a packet
+    if (msg->getKind() == 2) {
+        // msg is a FeedBackPkt
+        FeedBackPacket* feedbackPkt = (FeedBackPacket*)msg;
+
+        // Do something with the feedback info
+        int remainingBuffer = feedbackPkt->getRemainingBuffer();
+        // (...)
+
+        delete(msg);
+
+    } else if (msg->getKind() == 0) {
+        // msg is a data packet
+        // if msg is signaling an endServiceEvent
+        if (msg == endServiceEvent) {
+            // if packet in buffer, send next one
+            if (!buffer.isEmpty()) {
+                // dequeue packet
+                cPacket *pkt = (cPacket*) buffer.pop();
+                // send packet
+                send(pkt, "toOut$o");
+                // start new service
+                serviceTime = pkt->getDuration();
+                scheduleAt(simTime() + serviceTime, endServiceEvent);
+            }
+        // check buffer limit
+        } else if (buffer.getLength() >= par("bufferSize").intValue()) {
+            // drop the packet
+            delete msg;
+            this->bubble("packet dropped");
+            packetDropVector.record(1);
+        } else {
+            // enqueue the packet
+            buffer.insert(msg);
+            bufferSizeVector.record(buffer.getLength());
+            // if the server is idle
+            if (!endServiceEvent->isScheduled()) {
+                // start the service now
+                scheduleAt(simTime() + 0, endServiceEvent);
+            }
+        }
+    }
+}
+
+#endif /* TransportTx */
